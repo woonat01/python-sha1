d8b389b99a2c1cbb71d6333076519ec5b2d28d33
nwood <nwood01@unomaha.edu>
2017-11-10 20:39:38 -0600

    closest I've come to getting padding correct.

--- input.py
+++ input.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sha1  # credit for sha1 from https://github.com/ajalt/python-sha1
-import struct
+import sys
+
+print (sys.getdefaultencoding())
+
 # message = input("What is your message?\n")
 message = "comment1=Crypto%20Gurus;userdata=foo;comment2=%20hereAt%20UNO%20Omaha%20NE%20USA"
 print("Message:\n" + message)
@@ -9,12 +13,11 @@ print("Message:\n" + message)
 key = "A1852D554F5528A2A949FAC01087E6C9"
 print("Key:\n" + key)
 
-mac = bytes((key + message + "\n"), 'utf-8')
+mac = bytes((key + message), 'utf-8')
 # newline to accommodate the "echo" function specified in the original programs input.
 # Don't use echo to test - it doesnt interpret the url well. Pass url in text file.
 print("MAC: ")
 print(mac)
-print()
 
 origMac = sha1.sha1(mac)
 # call sha1.py sha1 function and pass mac as input
@@ -25,17 +28,37 @@ print("sha1 digest original key + message: " + origMac)
 newMessage = ";admin=true"
 macAttack2 = bytes((newMessage + "\n"), 'utf-8')
 # hashed with modified register
-extHash = sha1.sha1Modified(macAttack2)
-print("extHash:                            " + extHash)
+# extHash = sha1.sha1Modified(macAttack2)
+# print("extHash:                            " + extHash)
 
 
-keySize = len(key + "\n")
+keySize = len(key * 8)
 print("keySize: " + str(keySize))
-messageSize = len(message)
+messageSize = len(message * 8)
 print("messageSize: " + str(messageSize))
-macSize = len(mac)
+
+# length of mac in bits
+macSize = len(mac * 8)
 print("macSize: " + str(macSize))
-i = 1
+
+# pad in bits - convert to bytes
+pad = int((1024-(keySize+messageSize))/8)  # (macSize % 512)
+
+print("Pad Size: " + str(pad))
+# padding = start of padding + 0 padding * pad - leave length for beginning and length definition of pad
+# must present hex as binary "b'x'"
+# I have tried many combinations of pad lengths. Can't quite figure out what to put in as the pad.
+padding = b'\x80' + (b'\x00' * (pad - 0)) + b'\x80'
+
+previousMac = key + message
+
+macAttack = bytes(previousMac, 'utf-8') + padding + bytes(newMessage, 'utf-8')
+print("macAttack: " + str(macAttack))
+newMac = sha1.sha1(macAttack)
+print("newMac: " + str(newMac))
+
+
+'''i = 1
 for i in range (0, 512):
 
     pad = i  # (macSize % 64)
@@ -54,7 +77,4 @@ for i in range (0, 512):
         print("SUCCESS")
         break
     i += 1
-
-
-
-
+'''
